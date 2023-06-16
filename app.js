3e67a7d61f1781c87e216ddae7b0f67bea35e482
caroko9 <caroko9@hotmail.com>
2023-06-16 13:24:04 -0300

    footer images added

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ const app = express ();
 
 const path = require ('path');
 
+
+app.use(express.static(path.join(__dirname, './public')))
+
 app.get('/', function (req, res){
     res.sendFile(path.join(__dirname, '/views/index.html'));
     });
     
-app.get('/carrito', function (req, res){
-res.sendFile(path.join(__dirname, '/views/carrito.html'));
-});
-app.get('/register', function (req, res){
-    res.sendFile(path.join(__dirname, '/views/register.html'));
-    });
-app.use('/public', express.static(path.join(__dirname, 'public')));
-        
+
 app.listen(3004, () => console.log('Esto fue exitoso'));
+
+
