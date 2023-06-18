e4eee7ff0c12ae0a053ae34f2dd3d2e924dfd623
User <taquinibautista@gmail.com>
2023-06-17 23:01:30 -0300

    cambios en vista carrito

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express ();
 const path = require ('path');
 
 
-app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static(path.join(__dirname,'./public')))
 
 app.get('/', function (req, res){
     res.sendFile(path.join(__dirname, '/views/index.html'));
